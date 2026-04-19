1018ba5e5eb69a3b6f81b7ef3acc7fe1cac1193d
Pixel <scott.blatt.0b10@gmail.com>
2026-04-19 11:50:37 -0500

    fix silicon

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -4,11 +4,18 @@ const badRecipes = [
   "ae2:inscriber/logic_processor_print",
   "ae2:inscriber/engineering_processor_print",
   "ae2:inscriber/silicon_print",
-  "ae2:smelting/silicon_from_certus_quartz_dust"
+  "ae2:smelting/silicon_from_certus_quartz_dust",
+  "ae2:blasting/silicon_from_certus_quartz_dust",
 ];
 
 ServerEvents.recipes(event => {
   badRecipes.forEach((recipeId) => {
     event.remove({ id: recipeId });
   });
+  // Fix mods using ae2 silicon when it has been fully purged
+  event.replaceInput(
+    { input: "ae2:silicon" },
+    "ae2:silicon",
+    "#c:silicon"
+  );
 });
