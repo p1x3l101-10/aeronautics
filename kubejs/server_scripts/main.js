8072312bc3e06b7069958f421c9b1d48979d37e9
Pixel <scott.blatt.0b10@gmail.com>
2026-04-19 11:44:34 -0500

    ban recipes

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -1,4 +1,11 @@
-const badRecipes = []
+const badRecipes = [
+  "oritech:foundry/alloy/steel",
+  "ae2:inscriber/calculation_processor_print",
+  "ae2:inscriber/logic_processor_print",
+  "ae2:inscriber/engineering_processor_print",
+  "ae2:inscriber/silicon_print",
+  "ae2:smelting/silicon_from_certus_quartz_dust"
+];
 
 ServerEvents.recipes(event => {
   badRecipes.forEach((recipeId) => {
