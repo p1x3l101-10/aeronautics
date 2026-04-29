4fe342687208e9f227f8235bcb8f391f2c5ef4f8
Pixel <scott.blatt.0b10@gmail.com>
2026-04-28 23:17:31 -0500

    add cast iron recipe

---
+++ kubejs/server_scripts/createbigcannons/castIron.js
@@ -0,0 +1,25 @@
+const ingot_mb = 144;
+const ingot_processingTime = 180;
+
+ServerEvents.recipes(event => {
+  const foundryMelting = (input1, input2, processingTime, outputId, outputMb) => {
+    event.custom({
+      type: "createbigcannons:melting",
+      heat_requirement: "heated",
+      ingredients: [
+        {
+          tag: input1
+        },
+        {
+          tag: input2
+        }
+      ],
+      processing_time: processingTime,
+      results: [{
+        amount: outputMb,
+        id: outputId
+      }]
+    });
+  };
+  foundryMelting("c:ingots/iron", "c:coal", ingot_processingTime * 2, "createbigcannons:molten_cast_iron", ingot_mb * 2);
+});
