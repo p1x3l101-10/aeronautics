0b2b57456587013e63e60e0a2237273e47151106
Pixel <scott.blatt.0b10@gmail.com>
2026-05-06 09:50:04 -0500

    fix CBC

--- kubejs/server_scripts/createbigcannons/moltenMetals.js
+++ kubejs/server_scripts/createbigcannons/moltenMetals.js
@@ -1,22 +1,26 @@
 ServerEvents.recipes(event => {
   const ingot_mb = 144;
   const ingot_processingTime = 180;
+  const ingot_castingTime = 200;
 
   const types = {
     ingot: {
       mb: ingot_mb,
       tagPfx: "ingots",
-      processingTime: ingot_processingTime
+      processingTime: ingot_processingTime,
+      castingTime: ingot_castingTime
     },
     nugget: {
       mb: ingot_mb / 9,
       tagPfx: "nuggets",
-      processingTime: ingot_processingTime / 9
+      processingTime: ingot_processingTime / 9,
+      castingTime: ingot_castingTime / 9
     },
-    ingot: {
+    block: {
       mb: ingot_mb * 9,
       tagPfx: "storage_blocks",
-      processingTime: ingot_processingTime * 9
+      processingTime: ingot_processingTime * 9,
+      castingTime: ingot_castingTime * 9
     },
   };
 
@@ -27,14 +31,13 @@ ServerEvents.recipes(event => {
     "nethersteel"
   ];
 
-  const problemMetals = [
-    "nethersteel"
-  ];
-
   const defaultName = "createbigcannons";
   const nameOverrides = {
     steel: "tfmg",
-    "cast_iron": "tfmg"
+    cast_iron: "tfmg"
+  };
+  const liquidNameOverrides = {
+    steel: "tfmg"
   };
 
   const itemOverrides = {
@@ -56,18 +59,38 @@ ServerEvents.recipes(event => {
       }]
     });
   };
-  const foundryPressing = (input, outputId) => {
+  const foundryPressing = (inputTag, inputMb, outputId) => {
     event.custom({
       type: "create:compacting",
       heat_requirement: "heated",
-      ingredients: [input],
+      ingredients: [{
+        type: "neoforge:tag",
+        amount: inputMb,
+        tag: inputTag
+      }],
       results: [{
         id: outputId
       }]
     });
   };
+  const foundryCasting = (inputTag, inputMb, castingTime, outputId) => {
+    event.custom({
+      type: "tfmg:casting",
+      ingredients: [{
+        type: "neoforge:tag",
+        amount: inputMb,
+        tag: inputTag
+      }],
+      processing_time: castingTime,
+      results [{
+        id: outputId
+      }]
+    });
+  };
+  // Recipe editing loop
   metals.forEach((metal) => {
     const modNamespace = (metal in nameOverrides) ? nameOverrides[metal] : defaultName;
+    const modNamespaceLiquid = (metal in liquidNameOverrides) ? liquidNameOverrides[metal] : defaultName;
     for (const [type, typeMeta] of Object.entries(types)) {
       const itemNameProto = metal + "_" + type;
       const itemName = (itemNameProto in itemOverrides) ? itemOverrides[itemNameProto] : itemNameProto;
@@ -75,12 +98,25 @@ ServerEvents.recipes(event => {
       event.remove({ id: "createbigcannons:melting/melt_" + metal + "_" + type });
       event.remove({ id: "createbigcannons:compacting/forge_" + metal + "_" + type });
       // Recreations with corrected values
-      foundryMelting("c:" + typeMeta.tagPfx + "/" + metal, typeMeta.processingTime, "createbigcannons:molten_" + metal, typeMeta.mb);
-      if (problemMetals.includes(metal)) {
-        foundryPressing({ fluid: "createbigcannons:molten_" + metal, amount: typeMeta.mb, type: "neoforge:single" }, modNamespace + ":" + itemName);
-      } else {
-        foundryPressing({ tag: "c:molten_" + metal, amount: typeMeta.mb, type: "neoforge:tag" }, modNamespace + ":" + itemName);
-      }
+      foundryMelting("c:" + typeMeta.tagPfx + "/" + metal, typeMeta.processingTime, modNamespaceLiquid + ":molten_" + metal, typeMeta.mb);
+    }
+    // Add casting recipes (omitting steel because TFMG has that already)
+    const castItemNameProto = metal + "_ingot";
+    const castItemName = (castItemNameProto in itemOverrides) ? itemOverrides[castItemNameProto] : itemNameProto;
+    if (metal != "steel") {
+      foundryCasting("c:molten_" + metal, ingot_mb, ingot_castingTime, modNamespace + ":" + castItemName);
     }
   });
+  // Add a shitty recipe for cast iron so you aren't stuck in a bootstrap paradox
+  foundryPressing("c:molten_cast_iron", ingot_mb * 2, "tfmg:cast_iron_ingot");
+});
+
+// Fix molten metal tags
+ServerEvents.tags('fluid', event => {
+  event.add("c:molten_nethersteel", "createbigcannons:molten_nethersteel");
+});
+
+// Hide unused liquids
+RecipeViewerEvents.removeEntries('fluid', event => {
+  event.remove("createbigcannons:molten_steel");
 });
