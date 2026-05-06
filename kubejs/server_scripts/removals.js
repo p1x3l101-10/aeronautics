1e27497ab5e857ee42f8c5ef5189c325ab7f8fed
Pixel <scott.blatt.0b10@gmail.com>
2026-05-05 19:38:47 -0500

    make cbc in it's own world

--- kubejs/server_scripts/removals.js
+++ kubejs/server_scripts/removals.js
@@ -1,12 +1,6 @@
 const badRecipes = [
   "ae2:smelting/silicon_from_certus_quartz_dust",
   "ae2:blasting/silicon_from_certus_quartz_dust",
-  "ae2:inscriber/calculation_processor_press",
-  "ae2:inscriber/engineering_processor_press",
-  "ae2:inscriber/logic_processor_press",
-  "ae2:inscriber/silicon_press",
-  "tfmg:compacting/cast_iron",
-  "createbigcannons:mixing/alloy_steel",
   "createnuclear:mixing/steel"
 ];
 
@@ -14,4 +8,6 @@ ServerEvents.recipes(event => {
   badRecipes.forEach((recipeId) => {
     event.remove({ id: recipeId });
   });
+  // Overwrite silicon type
+  event.replaceInput({ input: "ae2:silicon" }, "ae2:silicon", "#c:ingots/silicon");
 });
