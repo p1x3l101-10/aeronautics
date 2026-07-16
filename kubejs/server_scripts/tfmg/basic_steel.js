e3318727d68528d3a113515ab5d67f4885ad21fb
Pixel <scott.blatt.0b10@gmail.com>
2026-07-16 15:41:36 -0500

    update pack

--- kubejs/server_scripts/tfmg/basic_steel.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.recipes(event => {
-  // Make the basic steel recipe use cast iron (techincally cheaper, but also more complex)
-  event.replaceInput(
-    { id: "tfmg:industrial_blasting/steel_from_dust" },
-    "create:crushed_raw_iron",
-    "#c:ingots/cast_iron"
-  );
-});
